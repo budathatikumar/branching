e400c7a5435ff5a23b3d3cbd486d30033e196ad4
budathatikumar <budathati@example.com>
2026-03-18 22:12:29 +0530

    file.java

---
+++ file.java
@@ -0,0 +1,17 @@
+class VariablesExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Sum is: " + sum);
+    }
+}class VariablesExample {
+	    public static void main(String[] args) {
+		            int a = 10;
+			            int b = 20;
+				            int sum = a + b;
+
+					            System.out.println("Sum is: " + sum);
+						        }
+}
